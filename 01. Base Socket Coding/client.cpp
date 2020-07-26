9ff251fa289588e2cc1dc671f28364c591da9d7b
Aroya <aroya@sina.com>
2020-07-27 00:29:27 +0800

    [01]Base Socket Coding
    Space -> Tab

--- 01. Base Socket Coding/client.cpp
+++ 01. Base Socket Coding/client.cpp
@@ -10,45 +10,45 @@
 
 int main(int argc, char *argv[])
 {
-    int sock;
-    sockaddr_in serv_addr;
-    char msg[30];
+	int sock;
+	sockaddr_in serv_addr;
+	char msg[30];
 
-    if (3 != argc)
-    {
-        printf("port: %s\n", argv[0]);
-        exit(1);
-    }
+	if (3 != argc)
+	{
+		printf("port: %s\n", argv[0]);
+		exit(1);
+	}
 
-    // Step 1 create socket
-    printf("socket\n");
-    if (-1 == (sock = socket(PF_INET, SOCK_STREAM, 0)))
-    {
-        printf("sock error\n");
-        exit(1);
-    }
+	// Step 1 create socket
+	printf("socket\n");
+	if (-1 == (sock = socket(PF_INET, SOCK_STREAM, 0)))
+	{
+		printf("sock error\n");
+		exit(1);
+	}
 
-    // Step 2 conect to socket
-    printf("connect\n");
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = htons(atoi(argv[2]));
-    if (-1 == connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)))
-    {
-        printf("connect error\n");
-        exit(1);
-    }
+	// Step 2 conect to socket
+	printf("connect\n");
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+	if (-1 == connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)))
+	{
+		printf("connect error\n");
+		exit(1);
+	}
 
-    // Step 3 read from socket
-    printf("read\n");
-    if (-1 == read(sock, msg, sizeof(msg) - 1))
-    {
-        printf("read error\n");
-        exit(1);
-    }
+	// Step 3 read from socket
+	printf("read\n");
+	if (-1 == read(sock, msg, sizeof(msg) - 1))
+	{
+		printf("read error\n");
+		exit(1);
+	}
 
-    printf("recv msg: %s\n", msg);
-    close(sock);
+	printf("recv msg: %s\n", msg);
+	close(sock);
 
-    return 0;
+	return 0;
 }
