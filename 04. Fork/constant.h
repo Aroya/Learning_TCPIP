f3d36181e4f6f72481e53d2729cfe7a0da168961
Aroya <aroya@sina.com>
2020-08-14 01:20:46 +0800

    [04]Fork
    TEST_ZOMBIE_WAITPID
    TEST_ZOMBIE_SIGNAL

--- 04. Fork/constant.h
+++ 04. Fork/constant.h
@@ -1,9 +1,9 @@
 #ifndef __CONSTANT_4__
 #define __CONSTANT_4__
 
-#define TEST_ZOMBIE_WAIT
+// #define TEST_ZOMBIE_WAIT
 // #define TEST_ZOMBIE_WAITPID
-// #define TEST_ZOMBIE_SIGNAL
+#define TEST_ZOMBIE_SIGNAL
 // #define TEST_ZOMBIE_SIGACTION
 
 #if (defined TEST_ZOMBIE_WAIT) \
